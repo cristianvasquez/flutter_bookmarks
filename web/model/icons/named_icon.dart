2b8bac849094a8f51c99396ba13c0a58c3adbe11
Cristian Vasquez <cristianvasquez@gmail.com>
2020-04-21 13:50:00 +0100

    Added font awesome icons

---
+++ web/model/icons/named_icon.dart
@@ -0,0 +1,36 @@
+import 'package:CardFLows/model/icons/all_icons.dart';
+import 'package:flutter/widgets.dart';
+
+class NamedIcon implements Comparable {
+  final IconData iconData;
+  final String name;
+
+  const NamedIcon(this.iconData, this.name);
+
+  @override
+  String toString() => 'IconDefinition{iconData: $iconData, name: $name}';
+
+  factory NamedIcon.fromMap(Map data) {
+    return allIcons[int.parse(data['iconId'])];
+  }
+
+  Map<String, dynamic> toJsonMap() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['iconId'] = '${allIcons.indexOf(this)}';
+    return data;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NamedIcon &&
+          runtimeType == other.runtimeType &&
+          iconData == other.iconData &&
+          name == other.name;
+
+  @override
+  int get hashCode => iconData.hashCode ^ name.hashCode;
+
+  @override
+  int compareTo(other) => name.compareTo(other.pane);
+}
